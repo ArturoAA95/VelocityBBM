f5eb13bc339e4acfa017a742eccf9340f64ce08d
Arturo AA <arturo.arellano@cimat.mx>
2025-02-10 23:50:36 -0500

    Succesful Convergence to principal eigenvalue

---
+++ tests/testConvEig.py
@@ -0,0 +1,38 @@
+import numpy as np
+from EigenProblem import PerOp_FD
+from EigenProblem import PerOp_FD_Sparse
+from BranchingRates import g_1
+import matplotlib.pyplot as plt
+from scipy.sparse import isspmatrix_dia
+from scipy.sparse import linalg
+from scipy import integrate
+
+
+e = [0,1]
+lamb = 0
+m = 25
+m_s = 10
+Eig = np.empty(m)
+
+
+r = integrate.nquad(g_1 , [[0,1], [0,1]])
+
+for i in range(m):
+    n = (i+1)*m_s
+    B = PerOp_FD_Sparse(n, e, lamb, g_1)
+    b = linalg.eigs(B, 1 , sigma=r[0])
+    Eig[i] = b[0][0]
+
+
+#print(r[0])
+#print(len(x))
+
+x = r[0]*np.ones(m)
+print(np.abs(r[0]-Eig[m-1] ))
+plt.plot(Eig)
+plt.plot(x)
+plt.show()
+
+
+
+
