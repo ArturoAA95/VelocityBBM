5daf0414276be3df4ecf673200bdca1cca226990
Arturo AA <arturo.arellano@cimat.mx>
2025-02-12 20:48:15 -0500

    failed

--- tests/testSave.py
+++ tests/testSave.py
@@ -6,41 +6,48 @@ from BranchingRates import g_3
 from scipy.sparse import linalg
 import matplotlib.pyplot as plt
 
-n = 100
-#e = [1/np.sqrt(2), 1/np.sqrt(2)]
+n = 200
 
-N = 4
-dim = 25
-angle = [i*(2*np.pi/N) for i in range(N)]
-lamb = [i*.08 for i in range(dim) ]
+N = 2
+dim = 20
+angle = [i*(np.pi/N) for i in range(N)]
+lamb = [(i+5)*.08 for i in range(dim) ]
 
 Gamma = np.empty((N, dim))
  
 for j in range(N):
-    print('e')
     e = [np.sin(angle[j]), np.cos(angle[j])]
-    
+    print(e)
     for i in range(dim):
-        r = lamb[i]**2/2 + 1
-        B = PerOp_FD_Sparse(n, e, lamb[i], g_2)
-        b = linalg.eigs(B, 1 , sigma=r, return_eigenvectors=False)
+        r = lamb[i]**2/2 + 6
+        B = PerOp_FD_Sparse(n, e, lamb[i], g_3)
+        b = linalg.eigs(B, 1 , return_eigenvectors=False)
+        print(b)
+        print(i)
         Gamma[j,i] = b[0]
     
-    print('b')
-    name = '{}'.format(j)
-    np.savez('{0}.npz'.format(name), lamb=lamb, Eig=Gamma[j,:])
-    print('a')
+    print(j)
 
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.title.set_text('{}_{}'.format(e[0], e[1]))
-    plt.xlim(0, 5)
-    plt.ylim(0, 5)
-    ax.plot(lamb, Gamma[j,:])
-    ax.axis('equal')
-    fig.savefig('{0}.png'.format(name))   # save the figure to file
-    print('c')
-    plt.close(fig)
-    print('d')
+print(np.linalg.norm(Gamma[0,:]-Gamma[1,:]))
+x = [i**2+1 for i in lamb]
+c_1 = [j/i for j,i in zip(Gamma[0,:], lamb)]
+c_2 = [j/i for j,i in zip(Gamma[1,:], lamb)]
+
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.title.set_text('{}_{}'.format(e[0], e[1]))
+plt.xlim(0, 5)
+plt.ylim(0, 5)
+#ax.plot(lamb, Gamma[0,:])
+#ax.plot(lamb, Gamma[1,:])
+ax.plot(lamb, c_1)
+ax.plot(lamb, c_2)
+ax.axis('equal')
+plt.show()
+#fig.savefig('{}.png'.format(j))   # save the figure to file
+plt.close(fig)
+
+#np.savez('test.npz', lamb=lamb, Gamma=Gamma)
+
     
 
 
